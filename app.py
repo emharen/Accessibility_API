ec3d5a99633566f5b6db2ff5de3031f6afc56aef
emharen <43094408+emharen@users.noreply.github.com>
2019-09-22 15:11:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 
-import sqlalchemy
+import sqlalchemy as db
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -17,47 +17,70 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///whitney.sqlite.sqlite")
 
-# reflect an existing database into a new model
-Base = automap_base()
+# # reflect an existing database into a new model
+# Base = automap_base()
 
-# reflect the tables
-Base.prepare(engine, reflect=True)
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
 
-# Save table reference to a variable
-yelp_info = Base.classes.yelp_info
+# print(str(Base.classes))
 
-# Create the session link
-session = Session(engine)
+# # Save table reference to a variable
+# Yelp_info = Base.classes.yelp_info
 
-
+# # Create the session link   
+# session = Session(engine)
 
 #################################################
 # Routes
 #################################################
+engine = db.create_engine("sqlite:///whitney.sqlite")
+connection=engine.connect()
+metadata=db.MetaData()
+Yelp_info=db.Table('yelpinfo', metadata, autoload=True, autoload_with=engine)
+print(Yelp_info.columns.keys())
+        
+    # Query the API table
+query = db.select([Yelp_info])
+ResultProxy=connection.execute(query)
+ResultSet=ResultProxy.fetchall()
+print(ResultSet[:1])
 
+restaurant = []
+
+for row in ResultSet:
+    (field1, RestaurantName, PhoneNumber, Address, States, Category) = row
+
+    zone_dict = {}
+    zone_dict["id"] = field1
+    zone_dict["Restaurant"] = RestaurantName
+    zone_dict["Phone"] = PhoneNumber
+    zone_dict["Address"] = Address
+    zone_dict["City"] = States
+    zone_dict["Category"] = Category
+    restaurant.append(zone_dict)
+
+print(restaurant)
 # API Route
 @app.route("/")
 def zones():
-    
-    # Query the API table
-    results = session.query(yelp_info.field1, yelp_info.RestaurantName, yelp_info.PhoneNumber, yelp_info.Address, yelp_info.States, yelp_info.Category).all()
+   
 
-    # Create a dictionary from the row data and append to a list of zone_data
-    restaurant = []
-    for field1, RestaurantName, PhoneNumber, Address, States, Category in results:
-        zone_dict = {}
-        zone_dict["id"] = field1
-        zone_dict["Restaurant"] = RestaurantName
-        zone_dict["Phone"] = PhoneNumber
-        zone_dict["Address"] = Address
-        zone_dict["City"] = States
-        zone_dict["Category"] = Category
-        restaurant.append(zone_dict)
+
+    # # Create a dictionary from the row data and append to a list of zone_data
+   
+    #     zone_dict = {}
+    #     zone_dict["id"] = field1
+    #     zone_dict["Restaurant"] = RestaurantName
+    #     zone_dict["Phone"] = PhoneNumber
+    #     zone_dict["Address"] = Address
+    #     zone_dict["City"] = States
+    #     zone_dict["Category"] = Category
+    #     restaurant.append(zone_dict)
 
     return jsonify(restaurant)
-   
+
 
 if __name__ == "__main__": 
     app.run()
